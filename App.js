37a0256c5d6197079b84cbdb45446b7a13ca895f
Vinicius Saes <viniciusaesouza@gmail.com>
2025-03-26 21:31:47 -0300

    feat: init header

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { Image } from "react-native";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 export default function App() {
     return (
         <View style={styles.container}>
             <View style={styles.header}>
+                <Ionicons name="menu" size={24} color="#ED145B" />
                 <Image source={require("./assets/fiap-logo.png")} />
             </View>
             <View>
-				<Text>Open up App.js to start working on your app!</Text>
-				<StatusBar style="auto" />
-			</View>
+                <StatusBar style="auto" />
+            </View>
         </View>
     );
 }
@@ -20,10 +21,14 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: "#111312",
-        alignItems: "center",
-        justifyContent: "center",
     },
     header: {
-        
+        flexDirection: "row",
+        justifyContent: "space-between",
+		alignItems: 'center',
+		paddingHorizontal: 20,
+		paddingTop: 20,
+		paddingBottom: 10,
+		backgroundColor: "#141819",
     },
 });
